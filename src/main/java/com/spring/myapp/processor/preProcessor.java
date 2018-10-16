1a694a6d9b619aaa2fd5321a09c1ee698603d7af
Sahil Chopra <sahilchopra11111@gmail.com>
2018-10-16 17:08:49 +0530

    -- Added exception class + updated processor class

--- src/main/java/com/spring/myapp/processor/preProcessor.java
+++ src/main/java/com/spring/myapp/processor/preProcessor.java
@@ -1,18 +1,35 @@
 package com.spring.myapp.processor;
 
+import java.util.Date;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class preProcessor implements Processor {
+	
+	private static final Logger log = LoggerFactory.getLogger(preProcessor.class);
 
 	@Override
-	public void process(Exchange arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void process(Exchange exchange) throws Exception {
 		
-		System.out.println("In PreProcessor...");
+
+		log.info("STARTED PROCESSING MESSAGE : {} AT TIME: {}", exchange.getExchangeId(), new Date().toString());
+
+		Object messageBody = exchange.getIn().getBody();
+		if (messageBody == null) {
+
+			log.warn("Message body is null , unexpected condition . Nothing to do.");
+
+			// + throw Exception
+		}
 		
+		log.info("Message Body :"+ (String) messageBody);
+		 
 	}
 
 }
