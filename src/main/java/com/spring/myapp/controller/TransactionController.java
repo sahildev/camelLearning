0b72099fe8e6c4a2c0572f3787a7d34e671f9a77
Sahil Chopra <sahilchopra11111@gmail.com>
2018-11-08 17:55:12 +0530

    Merge conflict

--- src/main/java/com/spring/myapp/controller/TransactionController.java
+++ src/main/java/com/spring/myapp/controller/TransactionController.java
@@ -24,7 +24,6 @@ public class TransactionController {
 		Map<String,String> TriggerPathMap = new HashMap();
 		TriggerPathMap.put("path", "Triggered csv parser");
 		System.out.println("abc");
-		System.out.println("xyz");
 		producerTemplate.sendBody("direct:transactionProcessingRoute",TriggerPathMap);
 	}
 }
